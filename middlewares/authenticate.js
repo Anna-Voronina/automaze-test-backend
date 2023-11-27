ded14134b9ef1027a2dfa970d4363c74ef802c3a
Anna-Voronina <anna.vs2512@gmail.com>
2023-11-27 20:35:02 +0200

    update authenticate.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,7 +1,7 @@
 const { HttpError } = require("../helpers");
 
 const authenticate = async (req, res, next) => {
-  const { userId } = req.body;
+  const { userId } = req.query;
 
   if (!userId) {
     next(HttpError(401));
